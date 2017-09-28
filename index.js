09c22afb48404d1e8797f69fb1ca7a9f58555e06
Aubron Wood <aubron@Aubrons-MacBook-Pro.local>
2017-09-28 12:04:51 -0400

    new gitignore

---
+++ index.js
@@ -0,0 +1,79 @@
+var Client                = require('castv2-client').Client;
+var DefaultMediaReceiver  = require('castv2-client').DefaultMediaReceiver;
+var mdns                  = require('mdns');
+var request = require('request');
+
+var youtubedl = require('youtube-dl');
+
+
+var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+
+browser.on('serviceUp', function(service) {
+  console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
+  ondeviceup(service.addresses[0]);
+  browser.stop();
+});
+
+browser.start();
+
+function ondeviceup(host) {
+
+  var client = new Client();
+
+  client.connect(host, function() {
+    console.log('connected, launching app ...');
+
+    client.launch(DefaultMediaReceiver, function(err, player) {
+
+      player.on('status', function(status) {
+        console.log('status broadcast playerState=%s', status.playerState);
+      });
+
+      getVideo(player);
+
+    });
+
+  });
+
+  client.on('error', function(err) {
+    console.log('Error: %s', err.message);
+    client.close();
+  });
+
+}
+
+
+function getVideo(player) {
+  request('https://www.pornhub.com/webmasters/search?id=44bc40f3bc04f65b7a35&search=hard&tags[]=Teen&thumbsize=medium',
+  function(error,response,body) {
+    var videos = JSON.parse(body)['videos'];
+
+    var url = videos[0]['url'];
+        // Optional arguments passed to youtube-dl.
+    var options = [];
+
+      youtubedl.getInfo(url, options, function(err, info) {
+        if (err) throw err;
+        var media = {
+          // Here you can plug an URL to any mp4, webm, mp3 or jpg file with the proper contentType.
+          contentId: info.url,
+          contentType: 'video/mp4',
+          streamType: 'BUFFERED', // or LIVE
+
+          // Title and cover displayed while buffering
+          metadata: {
+            type: 0,
+            metadataType: 0,
+            title: "Big Buck Bunny",
+            images: [
+              { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg' }
+            ]
+          }
+        };
+
+        player.load(media, { autoplay: true }, function(err, status) {});
+        console.log('url:', info.url);
+      });
+
+    });
+}
